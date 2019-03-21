5b1f626e2b0654728ef5379308974dea2f1556ec
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-03-21 01:57:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -60,7 +60,9 @@ class Song
   end 
     
   def self.create_from_filename(file_name)
-    self.new_from_filename(file_name)
+    song = self.new
+    song.name = file_name.split(" - ")[1].split(".")[0]
+    song.artist_name = file_name.split(" - ")[0]
     song.save
     song
   end 
